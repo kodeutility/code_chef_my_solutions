e12fe767641588e7ddbd80300fdc437c06295899
Kiran <kodeutility@gmail.com>
2019-03-09 23:23:56 +0530

    number mirror solution added

---
+++ beginner_practice_problems/number_mirror.py
@@ -0,0 +1,8 @@
+# CodeChef          : https://www.codechef.com/users/kodeutility
+# GitHub            : https://www.github.com/kodeutility
+# Portfolio website : https://kodeutility.github.io/
+# Author            : Kiran BG
+
+if __name__ == '__main__':
+
+    print(int(input()))
