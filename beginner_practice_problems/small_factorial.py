c1f6a873f0ce5eda58139ab6569c1a6e586e0392
Kiran <kodeutility@gmail.com>
2019-03-10 00:25:58 +0530

    small factorial solution added

---
+++ beginner_practice_problems/small_factorial.py
@@ -0,0 +1,14 @@
+# CodeChef          : https://www.codechef.com/users/kodeutility
+# GitHub            : https://www.github.com/kodeutility
+# Portfolio website : https://kodeutility.github.io/
+# Author            : Kiran BG
+#Question link      : https://www.codechef.com/problems/FLOW018
+
+for _ in range(int(input())):
+    number = int(input())
+    def fact(number):
+        if number==1:
+            return 1
+        else:
+            return number * fact(number-1)
+    print(fact(number))
